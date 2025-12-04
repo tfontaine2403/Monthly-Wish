bf45a4519ad227174cdd2e5ab6d8f3198bf9b7ad
JessicaD3 <jessica.dubois1003@gmail.com>
2025-12-04 11:52:45 +0100

    Correction BUG start

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-
 
 const apiRoutes = require('./routes/api');
 
@@ -13,17 +11,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-
 app.use((req, res, next) => {
   console.log(`${new Date().toLocaleTimeString()} ${req.method} ${req.url}`);
   next();
 });
 
-
 app.use('/api', apiRoutes);
 
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
